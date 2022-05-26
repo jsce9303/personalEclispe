ca890e7ca8e1d1f31300ab75def8c3117cfd5418
DESKTOP-U4O9AAT\MIT-302-02 <jsce9303@naver.com>
2022-05-26 17:43:04 +0900

    test1

--- chap06/src/sec06/other/One2.java
+++ chap06/src/sec06/other/One2.java
@@ -4,6 +4,6 @@ import sec06.One;
 public class One2 extends One {
 	void print() {
 		System.out.println(child); // 다른패키지 소속이나 자식클래스이기 때문에 public 및 protected멤버에만 접근 가능 11
-		System.out.println(anybody);//깃테스트
+		System.out.println(anybody);
 	}
 }
