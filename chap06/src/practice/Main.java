5a6d8c522ccb289c6c4431d5bf43e85117b156d0
DESKTOP-U4O9AAT\MIT-302-02 <jsce9303@naver.com>
2022-06-17 14:20:02 +0900

    0617

--- chap06/src/practice/Main.java
+++ chap06/src/practice/Main.java
@@ -3,26 +3,18 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {	
 		Scanner sc = new Scanner(System.in);
-		for(;;) {
-			String str = sc.nextLine();
-			int sum = 0; 
-			if(str.equals("#")) {
-                break;
-            }
-			str = str.toLowerCase();
-			for(int i = 0; i < str.length(); i++) {
-				switch(str.charAt(i)) {
-					case 'a' :
-					case 'e' :
-					case 'i' :
-					case 'o' :
-					case 'u' :
-						sum++;
-					default:
-						break;
-				}
-			}
-			System.out.println(sum);
+		int n = sc.nextInt();
+		double m = sc.nextInt();
+		double o,p,q = 0;
+		int r = 0;
+		
+		for(int i = n-1; i >0; i--) {
+			o  = q/(Math.pow(10, i));
+			q = m%(Math.pow(10, i));
+			System.out.println((int)o);
+			System.out.println((int)q);
 		}
-}
+		
+		
+	}	
 }
