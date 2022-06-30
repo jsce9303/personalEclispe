39a115429efa5685d6ebc2184c860a7696ce24e0
DESKTOP-U4O9AAT\MIT-302-02 <jsce9303@naver.com>
2022-06-30 17:46:57 +0900

    220630

--- chap06/src/practice/Main2.java
+++ chap06/src/practice/Main2.java
@@ -3,11 +3,12 @@ import java.util.*;
 public class Main2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int A = 0,B = 0;
-		while(A==0 || B == 0) {
-			A = sc.nextInt();
-			B = sc.nextInt();
-			System.out.println(A+B);
+		int N = sc.nextInt();
+		for(int i = 1; i <= N; i ++) {
+			for(int j =1; j <= i; j++) {
+				System.out.printf("*");
+			}
+			System.out.println();
 		}
-    }
+	}
 }
